dcf72ea8dac00d52506c879a98f36c13c574f742
Robin Kanters <robin@robinkanters.nl>
2016-08-24 20:28:39 +0200

    Added file component that reads file contents based on the filename from the payload

---
+++ src/main/java/com/robinkanters/athena/dataflow/component/FileComponent.java
@@ -0,0 +1,21 @@
+package com.robinkanters.athena.dataflow.component;
+
+import com.robinkanters.athena.dataflow.component.file.FileReader;
+
+@SuppressWarnings("WeakerAccess")
+public class FileComponent implements FlowComponent {
+    private final FileReader fileReader;
+
+    public FileComponent(FileReader fileReader) {
+        this.fileReader = fileReader;
+    }
+
+    public String read(String fileName) {
+        return fileReader.read(fileName);
+    }
+
+    @Override
+    public String run(String payload) {
+        return read(payload);
+    }
+}
