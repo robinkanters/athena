e4a1a7c51aaf4b909faee45a0bf0f52b5c59a2e2
Robin Kanters <robin@robinkanters.nl>
2016-08-24 20:46:35 +0200

    Added some missing tests

--- src/main/java/com/robinkanters/athena/datamapping/Variables.java
+++ src/main/java/com/robinkanters/athena/datamapping/Variables.java
@@ -7,6 +7,11 @@ import java.util.HashMap;
 public class Variables extends HashMap<String, String> implements KeyValueStore {
     private DecimalFormatter formatter = new DecimalFormatter();
 
+    public Variables() {}
+    public Variables(DecimalFormatter formatter) {
+        this.formatter = formatter;
+    }
+
     public void put(String key, int value) {
         put(key, value + "");
     }
