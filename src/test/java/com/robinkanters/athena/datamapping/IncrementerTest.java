8d9280bf9ed393db8bca5073e6c0cd4048230326
Robin Kanters <robin@robinkanters.nl>
2016-08-27 14:48:30 +0200

    Added some incrementer tests

--- src/test/java/com/robinkanters/athena/datamapping/IncrementerTest.java
+++ src/test/java/com/robinkanters/athena/datamapping/IncrementerTest.java
@@ -8,6 +8,10 @@ import static org.junit.Assert.assertEquals;
 public class IncrementerTest {
     private Incrementer incrementer;
 
+    private void assertNextValueIs(int expected) {
+        assertEquals(expected, incrementer.getNextValue());
+    }
+
     @Before
     public void setUp() throws Exception {
         incrementer = new Incrementer();
@@ -15,28 +19,42 @@ public class IncrementerTest {
 
     @Test
     public void valuesAreSequential() throws Exception {
-        assertEquals(0, incrementer.getNextValue());
-        assertEquals(1, incrementer.getNextValue());
+        assertNextValueIs(0);
+        assertNextValueIs(1);
     }
 
     @Test
     public void incrementerWithStartValueReturnsThatAsFirstValue() throws Exception {
         incrementer = new Incrementer(4);
-        assertEquals(4, incrementer.getNextValue());
+        assertNextValueIs(4);
     }
 
     @Test
     public void incrementerWithStartValueReturnsIncrementalIntegers() throws Exception {
         incrementer = new Incrementer(2);
-        assertEquals(2, incrementer.getNextValue());
-        assertEquals(3, incrementer.getNextValue());
-        assertEquals(4, incrementer.getNextValue());
+        assertNextValueIs(2);
+        assertNextValueIs(3);
+        assertNextValueIs(4);
     }
 
     @Test
     public void incrementerWithStepValueReturnsIntegersThatDifferByThatAmount() throws Exception {
         incrementer = new Incrementer(1, 2);
-        assertEquals(1, incrementer.getNextValue());
-        assertEquals(3, incrementer.getNextValue());
+        assertNextValueIs(1);
+        assertNextValueIs(3);
+    }
+
+    @Test
+    public void greaterStartValue() throws Exception {
+        incrementer = new Incrementer(10, 2);
+        assertNextValueIs(10);
+        assertNextValueIs(12);
+    }
+
+    @Test
+    public void greaterStepValue() throws Exception {
+        incrementer = new Incrementer(4, 5);
+        assertNextValueIs(4);
+        assertNextValueIs(9);
     }
 }
