4b4b50f6803c466e4ad5a9c11964ee3886f1a094
Robin Kanters <robin@robinkanters.nl>
2016-08-29 14:39:22 +0200

    Made default implementation for FileReader using Apache Commons IO

---
+++ src/main/java/com/robinkanters/athena/dataflow/component/file/FileReaderImpl.java
@@ -0,0 +1,30 @@
+package com.robinkanters.athena.dataflow.component.file;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class FileReaderImpl implements FileReader {
+    public String read(String filename) {
+        try {
+            return tryRead(filename);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String tryRead(String filename) throws IOException {
+        File f = getFileForFilename(filename);
+        return readFile(f);
+    }
+
+    protected File getFileForFilename(String filename) {
+        return new File(filename);
+    }
+
+    protected String readFile(File f) throws IOException {
+        return FileUtils.readFileToString(f, Charset.defaultCharset());
+    }
+}
